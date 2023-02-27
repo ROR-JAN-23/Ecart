32442a8b30cfb93a5db43a0ba7311c01584a9ad1
Roadsingh <roadsingh@preciousinfosystem.com>
2023-02-27 15:07:15 +0530

    added mailer for forget password and account confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
 
   # Defines the root path route ("/")
   # root "articles#index"
